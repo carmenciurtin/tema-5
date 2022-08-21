ec1d6f423a84b305be862888781d3f6091f3414c
carmen ciurtin <carmenciurtin@yahoo.com>
2022-08-21 14:12:54 +0300

    teme 6 final

--- src/test/java/tests/WebTestPages.java
+++ src/test/java/tests/WebTestPages.java
@@ -8,39 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebTestPages {
 
     public static void main(String[] args){
+
         testWebTestPages();
     }
     public static void testWebTestPages(){
         ChromeDriver driver = WebDriverManager.createChromeDriver();
-        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        driver.get("https://demoqa.com/elements");
 
-        WebElement title = driver.findElement(By.tagName("h1"));
+        WebElement title = driver.findElement(By.cssSelector(".main-header"));
         System.out.println(title.getText());
 
-        WebElement text = driver.findElement(By.tagName("h2"));
-        System.out.println(text.getText());
-
-        WebElement text1 = driver.findElement(By.cssSelector("p.inline-explanation"));
-        System.out.println(text1.getText());
-
-        WebElement text2 = driver.findElement(By.cssSelector("a#basicpagetest"));
-        System.out.println(text2.getText());
-        text2.click();
-
-        WebElement text3 = driver.findElement(By.cssSelector("a#elementattributestest"));
-        System.out.println(text3.getText());
-        text3.click();
-
-        WebElement text4 = driver.findElement(By.cssSelector("a#findbytest"));
-        System.out.println(text4.getText());
-        text4.click();
-
-        //WebElement text5 = driver.findElement(By.cssSelector("#HTML Tag Examples"));
-        //System.out.println(text5.getText());
-
-        WebElement text6 = driver.findElement(By.cssSelector("a#tablestest"));
-        System.out.println(text6.getText());
-        text6.click();
+        //WebElement text = driver.findElement(By.tagName("h2"));
+        //System.out.println(text.getText());
+        //text.click();
 
         //driver.quit();
     }
